b092f995faa6f5edd890bd6ca88132e300ae78fb
ZHOUXINWEN <94298878@qq.com>
2019-01-02 16:36:00 +0800

    update

--- basenet/test.py
+++ basenet/test.py
@@ -1,13 +1,10 @@
 import torch
 import torch.nn as nn
-from resnext import CifarResNeXt, ShallowResNeXt
-from densenet import DenseNet
-from SimpleNet import SimpleNet
+
+from NetworkFactory import NetworkFactory
 import torch.backends.cudnn as cudnn
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
 cudnn.benchmark = True
-#model = SimpleNet(17).cuda()
-#model = CifarResNeXt(num_classes = 17, depth = 29, cardinality = 8).cuda()
-#model = ShallowResNeXt(num_classes = 17, depth = 11, cardinality = 16, UseGN = True) 
-model = DenseNet(num_classes = 17).cuda()   
+
+model = NetworkFactory.ConsturctNetwork('se_resnext50_32x4d', None).cuda()
 print(model(torch.randn(32,8,32,32).float().cuda(),torch.randn(32,10,32,32).float().cuda()))
